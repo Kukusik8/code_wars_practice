dc45d729ac0a55c31f9eea606668fd537c9560ba
Ruslan Sakhautdinov <sakhrus14061999@gmail.com>
2023-10-19 22:55:26 +0700

    ain't done

---
+++ 100 day of CodeWars/2_day(19.10.23)/oneRepeat.js
@@ -0,0 +1,9 @@
+// Given a string, you have to return a string in which
+// each character(case -sensitive) is repeated once
+// У вас есть строка, и вам нужно вернуть строку, в которой каждый символ (с учетом регистра) повторяется один раз.
+// * "String"      -> "SSttrriinngg"
+
+function doubleChar(str) {
+        
+
+}
